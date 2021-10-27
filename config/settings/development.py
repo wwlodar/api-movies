6f3663ae5c796f5f10c47b66e13def08ed0e1cf9
wwlodar <58707277+wwlodar@users.noreply.github.com>
2021-10-27 15:32:23 +0200

    adding STATIC_ROOT

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,3 +3,4 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
--- config/settings/production.py
+++ config/settings/production.py
@@ -3,3 +3,5 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
