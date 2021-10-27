bae24e83a4e96bcc454a287c92f8da849e53b243
wwlodar <58707277+wwlodar@users.noreply.github.com>
2021-10-27 16:12:17 +0200

    Revert "Update Procfile"
    This reverts commit 2784b9f441800739ddb561a056f56a9fc3994b63, reversing
    changes made to 8a7210a3d7ab3a0ef0b0c9d6d4b81ed131e0ae8a.

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,15 +3,3 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-INSTALLED_APPS = [
-  'django.contrib.admin',
-  'django.contrib.auth',
-  'django.contrib.contenttypes',
-  'django.contrib.sessions',
-  'django.contrib.messages',
-  'django.contrib.staticfiles',
-  'api.apps.ApiConfig',
-  'rest_framework',
-  'django_filters'
-]
--- config/settings/production.py
+++ config/settings/production.py
@@ -5,18 +5,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS = [
-  'django.contrib.admin',
-  'django.contrib.auth',
-  'django.contrib.contenttypes',
-  'django.contrib.sessions',
-  'django.contrib.messages',
-  'django.contrib.staticfiles',
-  'api.apps.ApiConfig',
-  'rest_framework',
-  'django_filters',
-  'herokuapp'
-]
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
